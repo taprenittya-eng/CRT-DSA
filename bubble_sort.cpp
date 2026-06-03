7f61d125b85aae6c8ed6de203347138cdb1d58a8
taprenittya-eng <taprenittya@gmail.com>
2026-06-03 10:27:22 +0530

    Create bubble_sort.cpp

---
+++ bubble_sort.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	int arr[] = { 1,0,2,1,0,2 };
+	int n = 6;
+	for (int i = 1;i < n;i++) {
+		for (int j = 0;j < n - i;j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j],arr[j + 1]);
+			}
+		}
+	}
+	for (int i = 0;i < n;i++) {
+		cout << arr[i] << " ";
+	}
+}
+//0 0 1 1 2 2
